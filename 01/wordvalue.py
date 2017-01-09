e07a34f8d3d7d20d831dacabfc298877f84d9cf7
aisaacroth <aisaacroth@gmail.com>
2017-01-08 21:21:22 -0500

    make functions terser

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -10,16 +10,13 @@ def load_words():
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    return sum(LETTER_SCORES.get(letter, 0) for letter in word.upper())
+    return sum(LETTER_SCORES.get(letter.upper(), 0) for letter in word)
 
 
 def max_word_value(word_list=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    if not word_list:
-        word_list = load_words()
-
-    return max(word_list, key=calc_word_value)
+    return max(word_list or load_words(), key=calc_word_value)
 
 
 if __name__ == "__main__":
